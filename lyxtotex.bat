1b2d333e55c47ad551ecc25e4dd5354e4865da2f
yetisir <yetisir@gmail.com>
2016-05-05 12:14:44 -0400

    remves lyx files after conversion

--- lyxtotex.bat
+++ lyxtotex.bat
@@ -3,4 +3,5 @@ CD /D %~dp0
 SET Program="C:\Users\mmyetisi\AppData\Local\LyX 2.1\bin\lyx.exe"
 for %%A in (*.lyx) do %Program% --export  "latex"  %%A
 python removelyx.py
+RENAME *.lyx *.lyx2
 Pause
